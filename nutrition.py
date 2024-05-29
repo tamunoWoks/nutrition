feec291dbe6340e48492e70f45ac0d20a9b0a464
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 19:41:33 -0600

    Cleaned up code

--- nutrition.py
+++ nutrition.py
@@ -1,12 +1,25 @@
-item = input('Item: ').strip().title()
+item = input("Item: ").strip().title()
 fruits = {
-    'Apple':'130', 'Avocado':'50', 'Banana':'110', 'Grapefruit':'60',
-    'Grapes':'90', 'Honeydew Lemon':'50', 'Kiwifruit':'90', 'Lemon':'15',
-    'Lime':'20', 'Orange':'80', 'Peach':'60', 'Pear':'100', 'Pineapple':'50',
-    'Plums':'70', 'Strawberries':'50', 'Sweet Cherries':'100', 'Tangerine':'50'
+    "Apple": "130",
+    "Avocado": "50",
+    "Banana": "110",
+    "Grapefruit": "60",
+    "Grapes": "90",
+    "Honeydew Lemon": "50",
+    "Kiwifruit": "90",
+    "Lemon": "15",
+    "Lime": "20",
+    "Orange": "80",
+    "Peach": "60",
+    "Pear": "100",
+    "Pineapple": "50",
+    "Plums": "70",
+    "Strawberries": "50",
+    "Sweet Cherries": "100",
+    "Tangerine": "50",
 }
 
 for fruit in fruits:
     if fruit == item:
-        print(f'Calories: {fruits[item]}')
+        print(f"Calories: {fruits[item]}")
     continue
